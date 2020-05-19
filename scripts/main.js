8b78ce1c9fef3e90880a60761ed32ae5809548de
Mateo <mateo.valencia.g@gmail.com>
2020-05-19 03:18:58 -0500

    Audio and color bars changed

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 var audio = new Audio();
-audio.src = "./src/song.mp3";
+audio.src = "./src/audio.mp3";
 audio.controls = true;
 audio.loop = true;
 audio.autoplay = false;
@@ -10,19 +10,31 @@ var clicked = true;
 var analyserElement = document.querySelector('.mp3player__analyzerrender');
 var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
 
+var segundos = 0, color = "#FFB800", reloj;
+
 window.addEventListener("load", initMp3Player, false);
 
 var buttonStart = document.querySelector('.mp3player__button');
 var audioPlayer = document.querySelector('.mp3player__audiobox');
+var container = document.querySelector('.mp3player__infocontainer');
 
 buttonStart.addEventListener('click', function(){
-    var container = document.querySelector('.mp3player__infocontainer');
     container.style.display = 'none';
     context.resume();
 });
 
 audioPlayer.addEventListener('mouseover', function(){
     audioPlayer.classList.remove('mp3player__audiobox--selected');
+});
+
+audio.addEventListener('play', function(){
+    segundos = Math.trunc(audio.currentTime);
+    tiempo();
+});
+
+audio.addEventListener('pause', function(){
+    segundos = Math.trunc(audio.currentTime);
+    detenerTiempo();
 });
 
 function initMp3Player() {
@@ -62,7 +74,7 @@ function frameLooper() {
     analyser.getByteFrequencyData(fbc_array);
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#FFB800";
+    ctx.fillStyle = color;
     bars = 100;
 
     for (var i = 0; i - bars; i++) {
@@ -73,4 +85,36 @@ function frameLooper() {
         ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
     }
 }
+/* window.onload =  */
 
+function tiempo() {
+
+    reloj = setInterval(function(){
+        if(segundos >= 0 && segundos <= 59){
+            color = "#0A55C5";
+            frameLooper();
+        }
+        if(segundos >= 60 && segundos <= 179){
+            color = "#FFB800";
+            frameLooper();
+        }
+        if(segundos >= 180 && segundos <= 239){
+            color = "#0A55C5";
+            frameLooper();
+        }
+        if(segundos >= 240 && segundos <= 359){
+            color = "#FFB800";
+            frameLooper();
+        }
+        if(segundos >= 360 && segundos <= 420){
+            color = "#0A55C5";
+            frameLooper();
+        }
+        segundos = Math.trunc(audio.currentTime);
+        
+    },1000);
+}
+
+function detenerTiempo(){
+    clearInterval(reloj);
+}
